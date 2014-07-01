93b443ce525d1c6b30cbd153b53853fa61837844
Снегирев Дмитрий <rikkitp@gmail.com>
2014-07-01 14:18:19 +0400

    slider home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @slides = Slide.take 3
   end
 end
--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,4 +1,19 @@
+# == Schema Information
+#
+# Table name: slides
+#
+#  id                 :integer          not null, primary key
+#  image_file_name    :string(255)
+#  image_content_type :string(255)
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  title              :string(255)
+#  text               :text
+#  created_at         :datetime
+#  updated_at         :datetime
+#
+
 class Slide < ActiveRecord::Base
-  has_attached_file :image, styles: {admin_thumb: '128x128'}
+  has_attached_file :image, styles: {admin_thumb: '128x128', slide: '1051x616#'}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end

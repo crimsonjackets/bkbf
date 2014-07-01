7f0058957202dce0694b1eafbb5f0cdbab7b5db8
fishbullet <shindu666@gmail.com>
2014-07-01 15:25:06 +0400

    small fix

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -14,6 +14,8 @@
 #
 
 class Slide < ActiveRecord::Base
-  has_attached_file :image, styles: {admin_thumb: '128x128', slide: '1051x616#'}
+  has_attached_file :image, styles: {admin_thumb: '128x128', slide: '1051x616#'},
+                            url: "/public/images/slides/:id/:basename.:extension",
+                            path: ":rails_root/public/images/slides/:id/:basename.:extension"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
